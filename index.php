0f3001d2c34a0634c9f38bba1f581f39f1ec416d
Ruvick <rudikov.web@ya.ru>
2021-04-26 01:53:54 +0300

    Ссылки с главной на категории

--- index.php
+++ index.php
@@ -71,7 +71,7 @@
 
 				<div class="cards d-flex">
 
-					<a href="#" class="cards-item__img-l">
+					<a href="<?php echo get_category_link(13);?>" class="cards-item__img-l">  
 						<img src="<?php echo get_template_directory_uri();?>/img/card/card-01.png" alt="">
 						<div class="cards__text">
 							<h3>Военная <br>
@@ -83,7 +83,7 @@
 
 					<div class="cards-item__img-r d-flex">
 
-						<a href="#" class="cards__img cards__img-1">
+						<a href="<?php echo get_category_link(12);?>" class="cards__img cards__img-1">
 							<img src="<?php echo get_template_directory_uri();?>/img/card/card-02.png" alt="">
 							<div class="cards__text">
 								<h3>Форменная <br>
@@ -93,7 +93,7 @@
 							</div>
 						</a>
 
-						<a href="#"class="cards__img cards__img-2">
+						<a href="<?php echo get_category_link(9);?>" class="cards__img cards__img-2">
 							<img src="<?php echo get_template_directory_uri();?>/img/card/card-03.png" alt="">
 							<div class="cards__text">
 								<h3>Спецодежда
@@ -102,7 +102,7 @@
 							</div>
 						</a>
 
-						<a href="#"class="cards__img cards__img-3">
+						<a href="<?php echo get_category_link(14);?>" class="cards__img cards__img-3">
 							<img src="<?php echo get_template_directory_uri();?>/img/card/card-04.png" alt="">
 							<div class="cards__text">
 								<h3>Медицинская <br>
@@ -112,7 +112,7 @@
 							</div>
 						</a>
 
-						<a href="#" class="cards__img cards__img-4">
+						<a href="<?php echo get_category_link(3);?>" class="cards__img cards__img-4">
 							<img src="<?php echo get_template_directory_uri();?>/img/card/card-05.png" alt="">
 							<div class="cards__text">
 								<h3>Охота <br>
--- taxonomy-voencat.php
+++ taxonomy-voencat.php
@@ -6,48 +6,24 @@
 
 	<section class="category"> 
 		<div class="container">
-
 			<?php
 			if ( function_exists('yoast_breadcrumb') ) {
 				yoast_breadcrumb( '<p id="breadcrumbs">','</p>' );
 			}
 			?> 
-
+			
 			<h2><?php single_cat_title( '', true );?></h2>
 
 			<div class="prod-card d-flex"> 
-
 				<?php
 				while(have_posts()):
 					the_post();
 					get_template_part('template-parts/product-elem');  
 				endwhile;
 				?>
-
 			</div> 
 
-<!-- 			<nav class="pagination d-flex">
-				<div class="pagination__nav-links d-flex">
-					<a class="pagination__back" href="#"></a>
-					<span class="pagination__numbers">1</span>
-					<a class="pagination__numbers current" href="#">2</a>
-					<a class="pagination__numbers" href="#">3</a>
-					<a class="pagination__numbers" href="#">4</a>
-					<div class="pagination__block-dot d-flex">
-						<span class="pagination__dot">.</span>
-						<span class="pagination__dot">.</span>
-						<span class="pagination__dot">.</span>
-					</div>
-					<a class="pagination__numbers" href="#">10</a>
-					<a class="pagination__next" href="#"></a>
-				</div>
-			</nav>
-
-			<nav class="navigation pagination" role="navigation">
-			</nav> -->
-
 			<?php if ( function_exists( 'wp_corenavi' ) ) wp_corenavi(); ?>
-
 		</div>
 	</section>
 
