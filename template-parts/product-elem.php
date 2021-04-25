39eeb166262ecf41e09be2f2073c96304c351b89
Ruvick <rudikov.web@ya.ru>
2021-04-26 00:19:25 +0300

    Изменения в карточке товаров

--- template-parts/product-elem.php
+++ template-parts/product-elem.php
@@ -7,15 +7,24 @@
 	<a href="<?echo get_the_permalink(get_the_ID());?>" class="prod-card__text d-flex">
 		<h4><? the_title();?></h4>
 		<div class="prod-card__price d-flex">
-			<p class="prod-card__price-old"><?echo carbon_get_post_meta(get_the_ID(),"offer_old_price"); ?> руб.</p>
+			<?php
+				$price_old = carbon_get_post_meta(get_the_ID(),"offer_old_price");
+					if( strlen($price_old) == 0 ) { 
+						//echo '	Скидки нет';
+				} else if ( $price_old === 0 || $price_old === '0' ) {
+						//echo '<div class="availability-order">Скидки нет</div>';
+				} else {
+					echo "<p class='prod-card__price-old'> $price_old руб.</p>";
+				}
+			?>
 			<p class="prod-card__price-new"><?echo carbon_get_post_meta(get_the_ID(),"offer_price"); ?> руб.</p>
 		</div>
-		<p class="prod-card__benefit">ВЫГОДА: <span><?echo carbon_get_post_meta(get_the_ID(),"offer_benefit"); ?> РУБ.</span></p>
+		<p class="prod-card__benefit"><span><?echo carbon_get_post_meta(get_the_ID(),"offer_nal"); ?></span></p>
 	</a>
 
 	<div class="prod-card__button d-flex">
 		<a href="#" class="prod-card__baskcet d-flex">В корзину</a>
-		<a href="#" class="prod-card__order d-flex">Заказать</a>
+		<a href="<?echo get_the_permalink(get_the_ID());?>" class="prod-card__order d-flex">Подробнее</a>
 	</div>
 </div>
 
