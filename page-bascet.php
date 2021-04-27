cff1f82424686fa41c34ed72b05c228c9de24fe9
Ruvick <rudikov.web@ya.ru>
2021-04-27 17:21:08 +0300

    Стилизовал форму корзины

--- page-bascet.php
+++ page-bascet.php
@@ -75,7 +75,7 @@ get_header(); ?>
                 <div class = "form-line">
                     <label for = "id_fio">Имя, Фамилия*</label>
                     <div class="form-item">    
-                        <input id="id_fio" maxlength="255" name="fio" v-model="name" type="text" placeholder="Например, Александр">
+                        <input id="id_fio" class="bascet-form__input" maxlength="255" name="fio" v-model="name" type="text" placeholder="Например, Александр">
                         
                     </div>
                 </div>
@@ -83,7 +83,7 @@ get_header(); ?>
                 <div class="form-line">
                     <label for="id_email" id="p_id_email" class="form-label">E-mail*</label>
                     <div class="form-item">    
-                        <input id="id_email" maxlength="255" name="email" v-model="mail"  type="email" placeholder="alex-ivanov@gmail.com">
+                        <input id="id_email" class="bascet-form__input" maxlength="255" name="email" v-model="mail"  type="email" placeholder="alex-ivanov@gmail.com">
                         
                     </div>
                 </div>
@@ -91,7 +91,7 @@ get_header(); ?>
                 <div class="form-line">
                     <label for="id_phone" id="p_id_phone" class="form-label">Телефон*</label>
                     <div class="form-item">    
-                        <input id="id_phone" maxlength="255" name="phone" v-model="phone" type="tel" placeholder="+7-991-441-48-43">
+                        <input id="id_phone" class="bascet-form__input" maxlength="255" name="phone" v-model="phone" type="tel" placeholder="+7-991-441-48-43">
                         
                     </div>
                 </div>
@@ -99,7 +99,7 @@ get_header(); ?>
                 <div class="form-line">
                     <label for="id_address" id="p_id_address" class="form-label">Адрес</label>
                     <div class="form-item">    
-                        <textarea cols="40" id="id_address" name="address" v-model="adres" rows="10"></textarea>
+                        <textarea cols="40" class="bascet-form__textarea" id="id_address" name="address" v-model="adres" rows="10"></textarea>
                         <div class="form-help-text">ул. Строителей, д. 10, корп. 12, кв. 38, под. 3, код: 58а</div>
                     </div>
                 </div>
@@ -107,7 +107,7 @@ get_header(); ?>
                 <div class="form-line">
                     <label for="id_comment" id="p_id_comment" class="form-label">Комментарии</label>
                     <div class="form-item">    
-                        <textarea cols="40" id="id_comment" name="comment" v-model="comment" rows="10"></textarea>
+                        <textarea cols="40" class="bascet-form__textarea" id="id_comment" name="comment" v-model="comment" rows="10"></textarea>
                         <div class="form-help-text">Позвонить курьеру за час до выезда.</div>  
                     </div>
                 </div>
@@ -120,7 +120,7 @@ get_header(); ?>
                 </div>
 
                 <div class = "form_submit_line btn-wrapper">
-                    <button @click.self  = "sendBascet" type = "button" class = "btn">Оформить заказ</button>
+                    <button @click.self  = "sendBascet" type = "button" class = "bascet-form__btn btn">Оформить заказ</button>
                     <div v-show = "formNoValid" class = "no_feild">
                         Заполните все обязательные поля помеченные "*"
                     </div>
